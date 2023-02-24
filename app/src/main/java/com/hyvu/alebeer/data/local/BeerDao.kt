3117663a8a2b81fcc423664fcbf1938322cb628d
klienvu1998 <hyvu1998@gmail.com>
2023-02-25 00:28:22 +0700

    Implementing beer database

---
+++ app/src/main/java/com/hyvu/alebeer/data/local/BeerDao.kt
@@ -0,0 +1,20 @@
+package com.hyvu.alebeer.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.hyvu.alebeer.data.local.entity.BeerDbEntity
+
+@Dao
+interface BeerDao {
+    @Query("SELECT * FROM beer")
+    suspend fun getBeers(): List<BeerDbEntity>
+
+    @Query("SELECT * FROM beer WHERE id = :id")
+    fun getBeerById(id: String): BeerDbEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBeer(beer: BeerDbEntity): Long
+}
